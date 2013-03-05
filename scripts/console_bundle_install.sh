367c05b16e2624ea715a24fd9ee813df1abd7e62
Adam Miller <admiller@redhat.com>
2013-03-04 21:24:10 -0600

    all sorts of additions

---
+++ scripts/console_bundle_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Remove Gemfile.lock and bundle install on the local machine,
+# then recompile the rails assets
+pushd /var/www/openshift/console/
+  rm -f Gemfile.lock
+  bundle install
+  rm -fr tmp/cache/*
+  rake assets:precompile
+  chown -R ./*
+popd
+
---
+++ scripts/gen_broker_auth_pub_key.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! [[ -d /etc/openshift ]]; then
+  mkdir -p /etc/openshift
+fi
+
+if ! [[ -f /etc/openshift/server_pub.pem ]]; then
+  openssl genrsa -out /etc/openshift/server_priv.pem 2048 && \
+  openssl rsa -in /etc/openshift/server_priv.pem -pubout > \
+  /etc/openshift/server_pub.pem
+fi
