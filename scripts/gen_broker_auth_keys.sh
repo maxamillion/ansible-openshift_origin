5361ea1b6544ebb3d8d891f725dbe1a745e5d7ca
Adam Miller <admiller@redhat.com>
2013-03-04 21:33:48 -0600

    various cleanup, modifications, etc

--- scripts/gen_broker_auth_keys.sh
+++ scripts/gen_broker_auth_keys.sh
@@ -5,7 +5,10 @@ if ! [[ -d /etc/openshift ]]; then
 fi
 
 if ! [[ -f /etc/openshift/server_pub.pem ]]; then
-  openssl genrsa -out /etc/openshift/server_priv.pem 2048 && \
+  openssl genrsa -out /etc/openshift/server_priv.pem 2048 
+fi
+
+if ! [[ -f /etc/openshift/server_pub.pem ]]; then
   openssl rsa -in /etc/openshift/server_priv.pem -pubout > \
   /etc/openshift/server_pub.pem
 fi
